1c62aba857b00ce9fd49dbaa3ca22d03103fbc69
Akash <akashchandra111@gmail.com>
2018-04-17 15:10:54 +0530

    Bluetooth functionality added!

--- src/serial_comm/serial_comm.py
+++ src/serial_comm/serial_comm.py
@@ -1,15 +1,12 @@
-# from bluetooth import *
+from bluetooth import *
 
 class Bserial:
 	def __init__(self, hw_addr, port=1):
-		pass
-		# self.socket = BluetoothSocket(RFCOMM)
-		# self.socket.connect((hw_addr, port))
+		self.socket = BluetoothSocket(RFCOMM)
+		self.socket.connect((hw_addr, port))
 
 	def send(self, data):
-		print(data)
-		# self.socket.send(data)
+		self.socket.send(data)
 
 	def kill(self):
-		pass
-		# socket.close()
+		self.socket.close()
